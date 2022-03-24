f33831a5d65669f95f67f9b823b78b8e78c34993
songseunghyun <songsh96927@gmail.com>
2022-03-24 23:41:59 +0900

    달력추가(22.03.24)

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smartmirror_webview/topinfobar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class calendar extends StatefulWidget {
   const calendar({Key? key, this.weatherData}) : super(key: key);
@@ -11,6 +12,10 @@ class calendar extends StatefulWidget {
 }
 
 class _calendarState extends State<calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,11 +27,35 @@ class _calendarState extends State<calendar> {
           topinfobar(weatherData: widget.weatherData),
           Container(
             padding: EdgeInsets.all(40),
-            child: Text('Calendar',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white)),
+            child: SizedBox(
+              height: 400,
+              child: TableCalendar(
+                calendarBuilders: CalendarBuilders(
+                  dowBuilder: (context, day) {},
+                ),
+                focusedDay: DateTime.now(),
+                firstDay: DateTime.utc(2010, 1, 1),
+                lastDay: DateTime.utc(2040, 12, 31),
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                calendarFormat: _calendarFormat,
+                onFormatChanged: (format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                },
+                onPageChanged: (focusedDay) {
+                  _focusedDay = focusedDay;
+                },
+              ),
+            ),
           ),
         ],
       ),
